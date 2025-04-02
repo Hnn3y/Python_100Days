d11ca00031719333ae3ca1bc25b0ede38b65dd75
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-04-02 20:08:42 +0100

    build: Day 1 tasks

--- Day 1/string.py
+++ Day 1/string.py
@@ -1,1 +1,10 @@
-print("Hello world!\n Hello World!")
+print("Hello world!\nHello World!\nHello World")
+
+# Output 
+# Hello world!
+# Hello World!
+# Hello World 
+
+print("Hello World," + "my name is" + "Henney")
+
+# Hello World,my name isHenney
